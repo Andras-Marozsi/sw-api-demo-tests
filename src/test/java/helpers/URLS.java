dc628b6623e0bc2f11734b6c24fdd011d39ec3d4
Andras Marozsi <Andras_Marozsi@epam.com>
2018-11-04 09:21:27 -0500

    Add URLS helper

---
+++ src/test/java/helpers/URLS.java
@@ -0,0 +1,10 @@
+package helpers;
+
+public class URLS {
+    public static String SWAPI_BASE = "https://swapi.co/api";
+    public static String SWAPI_PLANETS = SWAPI_BASE + "/planets";
+
+    public static String getPlanetURL(int planetID) {
+        return SWAPI_PLANETS + "/" + planetID;
+    }
+}
--- src/test/java/tests/ExampleTests.java
+++ src/test/java/tests/ExampleTests.java
@@ -1,6 +1,8 @@
 package tests;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import helpers.URLS;
 
 import static io.restassured.RestAssured.*;
 
@@ -8,11 +10,12 @@ public class ExampleTests {
 
     @Test(priority = 1)
     public void planets_returns_200() {
-        given().when().get("http://swapi.co/api/planets").then().statusCode(200);
+        given().when().get(URLS.SWAPI_PLANETS).then().statusCode(200);
     }
 
+    @Parameters({"planetID"})
     @Test(priority = 1)
-    public void planet_returns_200() {
-        given().when().get("http://swapi.co/api/planets/1").then().statusCode(200);
+    public void planets_with_id_returns_200(int planetID) {
+        given().when().get(URLS.getPlanetURL(planetID)).then().statusCode(200);
     }
 }
